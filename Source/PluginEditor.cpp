0c2e3c77513accc0a37b6c461bcba55fe6389401
Soren <sorenmortimer@gmail.com>
2021-09-23 19:27:06 -0600

    Start Displaying Parameter Min/Max Values

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -67,12 +67,39 @@ void RotarySliderWithLabels::paint(juce::Graphics& g) {
 
     auto sliderBounds = getSliderBounds();
 
+
+
     /*g.setColour(Colours::red);
     g.drawRect(getLocalBounds());
     g.setColour(Colours::yellow);
     g.drawRect(sliderBounds);*/
 
     getLookAndFeel().drawRotarySlider(g, sliderBounds.getX(), sliderBounds.getY(), sliderBounds.getWidth(), sliderBounds.getHeight(), jmap(getValue(), range.getStart(), range.getEnd(), 0.0, 1.0), startAngle, endAngle, *this);
+
+    auto center = sliderBounds.toFloat().getCentre();
+    auto radius = sliderBounds.getWidth() * 0.5f;
+
+    g.setColour(Colours::white);
+    g.setFont(getTextHeight());
+
+    auto numChoices = labels.size();
+    for (int i = 0; i < numChoices; ++i) {
+        auto pos = labels[i].pos;
+        jassert(0.0f <= pos);
+        jassert(pos <= 1.0f);
+
+        auto angle = jmap(pos, 0.0f, 1.0f, startAngle, endAngle);
+
+        auto c = center.getPointOnCircumference(radius + getTextHeight() * 0.5 + 1, angle);
+
+        Rectangle<float> r;
+        auto str = labels[i].label;
+        r.setSize(g.getCurrentFont().getStringWidth(str), getTextHeight());
+        r.setCentre(c);
+        r.setY(r.getY() + getTextHeight());
+
+        g.drawFittedText(str, r.toNearestInt(), juce::Justification::centred, 1);
+    }
 }
 
 juce::Rectangle<int> RotarySliderWithLabels::getSliderBounds() const {
@@ -254,6 +281,10 @@ SimpleEQAudioProcessorEditor::SimpleEQAudioProcessorEditor(SimpleEQAudioProcesso
 {
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
+
+    peakFreqSlider.labels.add({ 0.0f, "20Hz" });
+    peakFreqSlider.labels.add({ 1.0f, "20kHz" });
+
     for (auto* comp : getComps()) {
         addAndMakeVisible(comp);
     }
