4133e0ddd453d1f744f94214d69a76521ea037c3
Soren <sorenmortimer@gmail.com>
2021-09-26 23:21:05 -0600

    Added Frequency Labels
    Thinned out Freq Amount

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -275,7 +275,7 @@ void ResponseCurveComponent::resized() {
     Graphics g(background);
 
     Array<float> freqs{
-        20, 30, 40, 50, 100, 200, 300, 400, 500, 1000, 2000, 3000, 4000, 5000, 10000, 20000
+        20, /*30, 40,*/ 50, 100, 200, /*300, 400,*/ 500, 1000, 2000, /*3000, 4000,*/ 5000, 10000, 20000
     };
 
     auto renderArea = getAnalysisArea();
@@ -312,6 +312,37 @@ void ResponseCurveComponent::resized() {
     }
 
     //g.drawRect(getAnalysisArea());
+
+    g.setColour(Colours::lightslategrey);
+
+    const int fontHeight = 10;
+    g.setFont(fontHeight);
+
+    for (int i = 0; i < freqs.size(); ++i) {
+        auto f = freqs[i];
+        auto x = xs[i];
+
+        bool addK = false;
+        String str;
+        if (f > 999.0f) {
+            addK = true;
+            f /= 1000.0f;
+        }
+
+        str << f;
+        if (addK)
+            str << "k";
+        str << "Hz";
+
+        auto textWidth = g.getCurrentFont().getStringWidth(str);
+
+        Rectangle<int> r;
+        r.setSize(textWidth, fontHeight);
+        r.setCentre(x, 0);
+        r.setY(1);
+
+        g.drawFittedText(str, r, juce::Justification::centred, 1);
+    }
 }
 
 juce::Rectangle<int> ResponseCurveComponent::getRenderArea() {
