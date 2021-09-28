d745138e6ffe9c24f297a7ae1a68ea1e51f39924
Soren <sorenmortimer@gmail.com>
2021-09-28 01:38:28 -0600

    Bypass Button Toggle Slider Use

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -14,10 +14,12 @@ void LookAndFeel::drawRotarySlider(juce::Graphics& g, int x, int y, int width, i
 
     auto bounds = Rectangle<float>(x, y, width, height);
 
-    g.setColour(Colour(39u, 77u, 171u));
+    auto enabled = slider.isEnabled();
+
+    g.setColour(enabled ? Colour(39u, 77u, 171u) : Colours::darkgrey);
     g.fillEllipse(bounds);
 
-    g.setColour(Colour(255u, 0u, 0u));
+    g.setColour(enabled ? Colour(255u, 0u, 0u) : Colours::grey);
     g.drawEllipse(bounds, 2.0f);
 
     if (auto* rswl = dynamic_cast<RotarySliderWithLabels*>(&slider)) {
@@ -543,6 +545,37 @@ SimpleEQAudioProcessorEditor::SimpleEQAudioProcessorEditor(SimpleEQAudioProcesso
     lowCutBypassButton.setLookAndFeel(&lnf);
     highCutBypassButton.setLookAndFeel(&lnf);
     analyzerEnabledButton.setLookAndFeel(&lnf);
+
+    auto safePtr = juce::Component::SafePointer<SimpleEQAudioProcessorEditor>(this);
+
+    peakBypassButton.onClick = [safePtr]() {
+        if (auto* comp = safePtr.getComponent()) {
+            auto bypassed = comp->peakBypassButton.getToggleState();
+
+            comp->peakFreqSlider.setEnabled(!bypassed);
+            comp->peakGainSlider.setEnabled(!bypassed);
+            comp->peakQualitySlider.setEnabled(!bypassed);
+
+        }
+    };
+
+    lowCutBypassButton.onClick = [safePtr]() {
+        if (auto* comp = safePtr.getComponent()) {
+            auto bypassed = comp->lowCutBypassButton.getToggleState();
+
+            comp->lowCutFreqSlider.setEnabled(!bypassed);
+            comp->lowCutSlopeSlider.setEnabled(!bypassed);
+        }
+    };
+
+    highCutBypassButton.onClick = [safePtr]() {
+        if (auto* comp = safePtr.getComponent()) {
+            auto bypassed = comp->highCutBypassButton.getToggleState();
+
+            comp->highCutFreqSlider.setEnabled(!bypassed);
+            comp->highCutSlopeSlider.setEnabled(!bypassed);
+        }
+    };
 
     setSize(600, 500);
 }
